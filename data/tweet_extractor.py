87dee1bc5c7efaa5764ff3d0f22c441b21bbc4d3
Alice Lam <lamalice9496@gmail.com>
2018-05-04 11:31:40 -0500

    Added crawled tweets

--- data/tweet_extractor.py
+++ data/tweet_extractor.py
@@ -1,17 +1,6 @@
 import json
 
-file = '/Users/alicelam/Kstate_Classes/CIS598/tweetf0rm/data/20180502/a4a2837cd4e241968c49496a8791dfce.json'
+file = '/home/alice/Developer/tweet-health-analytics/data/20180503/sample.json'
+data = json.loads(file)
 
-data = open(file, 'r')
-
-json_decode=json.load(file)
-
-def extractor():
-    for item in json_decode:
-        my_dict = {}
-        my_dict['title'] = item.get('labels').get('en').get('value')
-        my_dict['description'] = item.get('descriptions').get('en').get('value')
-        my_dict['id'] = item.get('id')
-        print(my_dict)
-
-extractor()
+unique_data = { each['full_text'] : each for each in data }.values()
